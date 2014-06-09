ef10e1582664a135d0f07e408dc4f7a58a7d2f5c
Po-Nan Li <lishantreasure@gmail.com>
2014-06-10 02:05:41 +0800

    fixed bug: GHIO improvment

--- gshrinkwrap.m
+++ gshrinkwrap.m
@@ -64,8 +64,8 @@ disp(['replica #' int2str(mx) ' with EF = ' num2str(my) ' selected']);
 R(:,:,1) = Rtmp(:,:,mx);
 
 % make Gaussian kernel
-x = (1:size(S,2)) - size(S,2)/2;
-y = (1:size(S,1)) - size(S,1)/2;
+x = (1:size(S,2)) - ceil(size(S,2)/2) - 1;
+y = (1:size(S,1)) - ceil(size(S,1)/2) - 1;
 [X, Y] = meshgrid(x, y);
 rad = sqrt(X.^2 + Y.^2);
 
@@ -73,15 +73,15 @@ rad = sqrt(X.^2 + Y.^2);
 % shrink-wrap
 for g = 1:gen
     Rmodel = R(:,:,g);
-    Rmodel( Rmodel < 0 ) = 0; % just in case
-    Rtmp( Rtmp < 0 ) = 0;
-    G = fft2(exp(-(rad./sqrt(2)./sig).^2));
+    % Rmodel( Rmodel < 0 ) = 0; % just in case
+    % Rtmp( Rtmp < 0 ) = 0;
+    
     parfor r = 1:rep
         % real-space improvement 
         Rtmp(:,:,r) = myalign( Rmodel, Rtmp(:,:,r) );
-        Rtmp(:,:,r) = sqrt( Rtmp(:,:,r) .* Rmodel );
+        Rtmp(:,:,r) = sign(Rmodel) .* sqrt( abs( Rtmp(:,:,r) .* Rmodel ) );
+        G = fft2(exp(-(rad./sqrt(2)./sig).^2));
         % make new support
-        
         Mtmp(:,:,r) = fftshift( ifft2( fft2(Rtmp(:,:,r)) .* G, 'symmetric') );
         Stmp(:,:,r) = ( Mtmp(:,:,r) >= cutoff2*max(max(Mtmp(:,:,r))) );
         % run hio
@@ -96,11 +96,11 @@ for g = 1:gen
     disp(['replica #' int2str(mx) ' with EF = ' num2str(my) ' selected']);
     R(:,:,g+1) = Rtmp(:,:,mx);
     Sup(:,:,g+1) = Stmp(:,:,mx);
-    
+    % smaller the kernel size
     if sig > 1.5
         sig = sig * 0.99;
     end
 end
 
-M = Mtmp(:,:,1);
+M = Mtmp(:,:,mx);
 
