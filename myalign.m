2b8506fcfc2ee309c07eeba57ab88cb75d57f282
Po-Nan Li <lishantreasure@gmail.com>
2014-06-09 11:55:06 +0800

    modify output message in myalign()

--- myalign.m
+++ myalign.m
@@ -4,12 +4,11 @@ function B = myalign(A, B)
     xcorr = fftshift( ifft2( fft2(A) .* conj(fft2( rot90(B,2) )) ) );
     [cr2, cc2, y2] = findpeaks2(xcorr);
     if y2(1) > y1(1)
-        disp('myalign: inverted');
         B = rot90(B, 2);
         cr2 = cr2(1) - round(size(A,1)/2);
         cc2 = cc2(1) - round(size(A,2)/2);
         B = circshift(B, [cr2, cc2]);
-        disp(['myalign: shifted (' int2str(cr2) ', ' int2str(cc2) ').']);
+        disp(['myalign: inverted and shifted (' int2str(cr2) ', ' int2str(cc2) ').']);
     else
         cr1 = cr1(1)- round(size(A,1)/2);
         cc1 = cc1(1) - round(size(A,2)/2);
