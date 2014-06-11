01f3f771983362359b3a459a2318d2efa134f738
Po-Nan Li <lishantreasure@gmail.com>
2014-06-11 17:46:00 +0800

    remove 'GM(GM<0)=0'

--- ghio2d.m
+++ ghio2d.m
@@ -6,16 +6,22 @@ function R = ghio2d(Fabs, S, n, gen, rep, checker, alpha)
     FG = zeros(size(Fabs,1), size(Fabs,2), rep);
     efs = zeros(rep, 1);
     sh = floor(sqrt(numel(find(S==1)))/2); % maximun shift
+    GM = zeros(size(Fabs));
+
     for g = 1:gen
         disp(['in generation #' int2str(g)]);
         if g == 1
             parfor r = 1:rep
                 G(:,:,r) = hio2d(Fabs, S, n, checker, alpha);
+                FG(:,:,r) = fft2( G(:,:,r) );
+                efs(r) = ef(Fabs, FG(:,:,r), checker);
             end
         else
             parfor r = 1:rep
+                % make new template
                 G(:,:,r) = myalign( GM, G(:,:,r) );
                 G(:,:,r) = sign( G(:,:,r) ) .* sqrt( abs(G(:,:,r) .* GM) );
+                % run independent HIO
                 G(:,:,r) = hio2d(fft2(G(:,:,r)), S, n, checker, alpha);
                 FG(:,:,r) = fft2( G(:,:,r) );
                 efs(r) = ef(Fabs, FG(:,:,r), checker);
@@ -23,9 +29,8 @@ function R = ghio2d(Fabs, S, n, gen, rep, checker, alpha)
         end
         % find the best replica
         [min_ef, min_ind] = min(efs);
-        disp(['replica1 #' int2str(min_ind) ' selected (EF = ' num2str(min_ef) ')']);
+        disp(['replica0 #' int2str(min_ind) ' selected (EF = ' num2str(min_ef) ')']);
         GM = G(:,:,min_ind);
-        GM(GM<0) = 0;
         R(:,:,g) = GM;
     end
    
