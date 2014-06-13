67c5a995663375361a1deb96ac848d93751dfceb
Po-Nan Li <lishantreasure@gmail.com>
2014-06-13 16:53:58 +0800

    use the sign of the template

--- ghio2d.m
+++ ghio2d.m
@@ -1,10 +1,10 @@
 % 2-D Guided-HIO written by Po-Nan Li @ Academia Sinica 2012
 % Reference: Chen et al., Phys. Rev. B. 76, 064113 (2007).
-function [R, G] = ghio2d(Fabs, S, n, gen, rep, checker, alpha)
+function [R, G, efs] = ghio2d(Fabs, S, n, gen, rep, checker, alpha)
     R = zeros(size(Fabs,1), size(Fabs,2), gen);
     G = zeros(size(Fabs,1), size(Fabs,2), rep);
     FG = zeros(size(Fabs,1), size(Fabs,2), rep);
-    efs = zeros(rep, 1);
+    efs = zeros(gen, rep);
     sh = floor(sqrt(numel(find(S==1)))/2); % maximun shift
     GM = zeros(size(Fabs));
 
@@ -14,21 +14,21 @@ function [R, G] = ghio2d(Fabs, S, n, gen, rep, checker, alpha)
             parfor r = 1:rep
                 G(:,:,r) = hio2d(Fabs, S, n, checker, alpha);
                 FG(:,:,r) = fft2( G(:,:,r) );
-                efs(r) = ef(Fabs, FG(:,:,r), checker);
+                efs(g,r) = ef(Fabs, FG(:,:,r), checker);
             end
         else
             parfor r = 1:rep
                 % make new template
                 G(:,:,r) = myalign( GM, G(:,:,r) );
-                G(:,:,r) = sign( G(:,:,r) ) .* sqrt( abs(G(:,:,r) .* GM) );
+                G(:,:,r) = sign( GM ) .* sqrt( abs(G(:,:,r) .* GM) );
                 % run independent HIO
                 G(:,:,r) = hio2d(fft2(G(:,:,r)), S, n, checker, alpha);
                 FG(:,:,r) = fft2( G(:,:,r) );
-                efs(r) = ef(Fabs, FG(:,:,r), checker);
+                efs(g,r) = ef(Fabs, FG(:,:,r), checker);
             end
         end
         % find the best replica
-        [min_ef, min_ind] = min(efs);
+        [min_ef, min_ind] = min(efs(g,:));
         disp(['replica #' int2str(min_ind) ' selected (EF = ' num2str(min_ef) ')']);
         GM = G(:,:,min_ind);
         R(:,:,g) = GM;
