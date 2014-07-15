220d9e8a32be78aaa21899671e0d15e2d35670ab
Po-Nan Li <lishantreasure@gmail.com>
2014-07-15 17:03:04 +0800

    create hiosupport.m

---
+++ hiosupport.m
@@ -0,0 +1,22 @@
+function Sup = hiosupport(support_size, image_size)
+
+% support dimenstion
+sp_h = support_size(1);
+if numel(support_size) > 1
+    sp_w = support_size(2);
+else
+    sp_w = support_size;
+end
+
+% image dimension
+img_h = image_size(1);
+if numel(image_size) > 1
+    img_w = image_size(2);
+else
+    img_w = image_size;
+end
+
+Sup = false(img_h, img_w);
+Sup(1:sp_h, 1:sp_w) = true;
+
+Sup = circshift(Sup, [round((img_h-sp_h)/2) round((img_h-sp_h)/2)]);
